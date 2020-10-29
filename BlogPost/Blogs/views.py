f7af29e4bfcba6eeff914b1bb21a311568a8ff6b
Akhil Soni <akhil0435@gmail.com>
2020-10-29 10:03:14 +0530

    Adding restrictions to user for certain function

--- BlogPost/Blogs/views.py
+++ BlogPost/Blogs/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from rest_framework import permissions
 from .models import Posts
@@ -7,41 +8,58 @@ from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import PostsSerializer, UserSerializer
+from .serializers import PostsSerializer
 # Create your views here.
 
 class PostListView(generics.ListAPIView):
-    queryset = Posts.objects.all()
     serializer_class = PostsSerializer
+    queryset = Posts.objects.all()
+
 
 class PostDetailView(generics.RetrieveAPIView):
     queryset = Posts.objects.all()
     serializer_class = PostsSerializer
 
-class PostsCreateView(generics.CreateAPIView):
+class PostsCreateView(UserPassesTestMixin, generics.CreateAPIView):
     serializer_class = PostsSerializer
     permission_classes = (permissions.IsAuthenticated,)
+
+    def test_func(self):
+        return self.request.user.is_active
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-class PostsDeleteView(generics.DestroyAPIView):
-    queryset = Posts.objects.all()
+class PostsDeleteView(UserPassesTestMixin, generics.DestroyAPIView):
+    def get_queryset(self):
+        if self.request.user.is_active:
+            username = self.request.user.username
+        queryset = Posts.objects.filter(writer=username)
+        return queryset
+
     permission_classes = (permissions.IsAuthenticated,)
+
+    def test_func(self):
+        return self.request.user.is_active
 
     def perform_destroy(self, instance):
-        instance.delete(user=self.request.user)
+        instance.delete()
 
-class PostUpdateView(generics.UpdateAPIView):
+class PostUpdateView(UserPassesTestMixin, generics.UpdateAPIView):
     serializer_class = PostsSerializer
-    queryset = Posts.objects.all()
     permission_classes = (permissions.IsAuthenticated,)
 
+    def test_func(self):
+        return self.request.user.is_active
+
+    def get_queryset(self):
+        if self.request.user.is_active:
+            username = self.request.user.username
+        queryset = Posts.objects.filter(writer=username)
+        return queryset
+
     def perform_update(self, serializer):
-        current_user = self.request.user
-        user = self.request.user
-        if user:
-            serializer.save(user=self.request.user)
+        serializer.save(user=self.request.user)
 
 class AuthView(APIView):
     permission_classes = (IsAuthenticated, )
